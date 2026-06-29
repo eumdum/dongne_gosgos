13b43af6c5450cd75c8bf53cacd3d04d845b4ea3
eumdum <qqq9395@naver.com>
2026-06-29 17:34:07 +0900

    괜히 gun어쩌고로 바꿧나보다.... 집가고싶다....

--- front/vite.config.js
+++ front/vite.config.js
@@ -8,8 +8,11 @@ export default defineConfig({
     vue(),
   ],
   server: {
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
     allowedHosts: ['all'],
-    host: true,
+    // host: true,
   },
   resolve: {
     alias: {
