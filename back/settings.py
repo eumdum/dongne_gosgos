4fe3019a757cc058c60e96955b0bda19efc5220d
eumdum <qqq9395@naver.com>
2026-06-30 16:33:01 +0900

    로컬에서도 테스트 가능하도록 수정

--- back/settings.py
+++ back/settings.py
@@ -111,16 +111,26 @@ WSGI_APPLICATION = "back.wsgi.application"
 
 hostname = socket.gethostname()
 
-DATABASES = { 
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST', default='db'),
-        'PORT': '3306',
+IS_DOCKER = env.bool('IS_DOCKER', default=False)
+
+if IS_DOCKER:
+    DATABASES = { 
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': env('DB_NAME'),
+            'USER': env('DB_USER'),
+            'PASSWORD': env('DB_PASSWORD'),
+            'HOST': env('DB_HOST'),
+            'PORT': env('DB_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
