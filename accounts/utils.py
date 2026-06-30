c2265734b2ec5cfcd92bcec8d264ce79b38829fc
eumdum <qqq9395@naver.com>
2026-06-30 15:36:02 +0900

    도커 수정완^^, 사업자번호조회 로직 수정

--- accounts/utils.py
+++ accounts/utils.py
@@ -8,18 +8,26 @@ def verify_business_number(b_no):
     service_key = env('BUSINESS_API_KEY')
     url = f"https://api.odcloud.kr/api/nts-businessman/v1/status?serviceKey={service_key}"
     
-    if b_no == "0000000000":  # 테스트용
-        return True
-        
+    if b_no == "0000000000": return True,  # 테스트용  
+
     payload = json.dumps({"b_no": [b_no]}) 
     headers = {'Content-Type': 'application/json'}
 
     try:
         response = requests.post(url, data=payload, headers=headers)
         res_data = response.json()
+
+        biz_info = res_data.get('data')[0]
+        status_code = biz_info.get('b_stts_cd') # 01:계속, 02:휴업, 03:폐업
+        status_msg = biz_info.get('b_stts')
+
+        if status_code == '01':
+            return True, status_msg
+        else:
+            return False, status_msg
         
-        if res_data.get('data')[0].get('b_stts_cd') == '01':  # 01이면 통과임
-            return True
-        return False
+        # if res_data.get('data')[0].get('b_stts_cd') == '01':  # 01이면 통과임
+        #     return True
+        # return False
     except:
-        return False
+        return False, "조회 오류"
